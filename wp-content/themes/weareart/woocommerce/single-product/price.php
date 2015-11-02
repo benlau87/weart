36c5b6ae0f414710f028d51a40b1978ec13883d0
benlau87 <benlau@gmx.net>
2015-11-02 05:34:22 +0100

    price

--- wp-content/themes/weareart/woocommerce/single-product/price.php
+++ wp-content/themes/weareart/woocommerce/single-product/price.php
@@ -16,16 +16,11 @@ global $product;
 $waa_only_print = get_post_meta($product->id, 'waa_only_print', true);
 $waa_original_price = get_post_meta($product->id, 'waa_original_price', true);
 
-print_r($product->product_type);
-
 if($product->product_type == 'variable')
 	$available_variations = $product->get_available_variations();
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-
-	
-		<?php 
-			if($waa_only_print == 'no' && $product->product_type == 'variable') { ?>
+<?php if($waa_only_print == 'no' && $product->product_type == 'variable') { ?>
 			<form class="variations_form cart" method="post" action="<?php bloginfo('url') ?>/checkout/" enctype="multipart/form-data">
 			<div class="single_variation_wrap" style="display: none;">
 				<input type="hidden" name="add-to-cart" value="<?php echo $product->id; ?>">
@@ -34,20 +29,17 @@ if($product->product_type == 'variable')
 				<input type="hidden" name="attribute_pa_print_material" value="original">
 				<input type="hidden" name="variation_id" value="<?=$available_variations[0]['variation_id']?>">
 			</div>
-			<p class="price">
 			<?php
-				echo '<span class="amount">'.number_format($waa_original_price, 2,',','.').'&nbsp;'.get_woocommerce_currency_symbol().'</span> <small class="woocommerce-price-suffix">'.$product->get_price_suffix().'</small></p>';
+				echo '<p class="price"><span class="amount">'.number_format($waa_original_price, 2,',','.').'&nbsp;'.get_woocommerce_currency_symbol().'</span> <small class="woocommerce-price-suffix">'.$product->get_price_suffix().'</small></p>';
 				echo '<button type="submit" class="single_add_to_cart_button button alt">'.__('Original in den Warenkorb', 'waa').'</button>';
 				echo '<div class="order-prints-line"><span>'.__('oder Abzüge bestellen', 'waa').'</span></div>';
 				echo '</form>';
 			}
 			else {
-				echo $product->get_price_html();
+				echo '<p class="price">'.$product->get_price_html().'</p>';
 			}
 		?>	    			
-	</p>
-
-		
+	</p>		
 	<meta itemprop="price" content="<?php echo $product->get_price(); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
 	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
